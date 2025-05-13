a0af0592265117f036fa0b3e309fc59204b00d10
AdiSatR7 <adisatr7@gmail.com>
2025-05-13 15:37:15 +0700

    fix: update readiness response to indicate service is ready

--- services/readiness.ts
+++ services/readiness.ts
@@ -5,6 +5,6 @@ export default class ReadinessService {
    * Check if the service is ready
    */
   static async isReady(req: Request, res: Response) {
-    res.status(200).json({ success: false, status: "ready" });
+    res.status(200).json({ success: true, status: "ready" });
   }
 }
