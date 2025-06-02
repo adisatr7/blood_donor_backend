60377ed2d43ba850edee51f2e6e1a44bd594bf58
AdiSatR7 <adisatr7@gmail.com>
2025-06-02 16:46:39 +0700

    feat: refactor GoogleSheetWorker to encapsulate synchronization logic

--- workers/googleSheetWorker.ts
+++ workers/googleSheetWorker.ts
@@ -4,7 +4,13 @@ import GoogleSheetService from "../services/googleSheetService";
 // Atur seberapa sering sistem Backend harus mengirim data ke Google Sheets
 const DURATION = "*/5 * * * *"; // 5 menit
 
-// Otomatis jalankan sinkronisasi data dengan Google Sheets
-cron.schedule(DURATION, () => {
-  GoogleSheetService.perform();
-});
+export default class GoogleSheetWorker {
+  /**
+   * Otomatis jalankan sinkronisasi data dengan Google Sheets
+   */
+  static start() {
+    cron.schedule(DURATION, () => {
+      GoogleSheetService.perform();
+    });
+  }
+}
