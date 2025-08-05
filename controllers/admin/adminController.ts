bd05a9dd71e3392ca162f3613848d0947fb9e07c
AdiSatR7 <adisatr7@gmail.com>
2025-08-05 09:57:30 +0700

    feat: add blood storage service and endpoint for admin

--- controllers/admin/adminController.ts
+++ controllers/admin/adminController.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import adminOnly from "../../middlewares/adminOnly";
+import AdminService from "../../services/admin/adminService";
+import BloodStorageController from "./bloodStorageController";
 import AppointmentController from "../appointmentController";
 import LocationController from "../locationController";
 import UserController from "./userController";
-import AdminService from "../../services/admin/adminService"
 
 const router = express.Router();
 
 router.post("/create-admin", AdminService.createAdmin); // Buat admin baru
 
-router.use("/users", UserController);
+router.use("/blood-storage", adminOnly, BloodStorageController);
+router.use("/users", adminOnly, UserController);
 router.use("/locations", adminOnly, LocationController);
 router.use("/appointments", adminOnly, AppointmentController);
 
---
+++ controllers/admin/bloodStorageController.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import validateRequestBody from "../../middlewares/validateRequestBody"
+import BloodStorageService from "../../services/admin/bloodStorageService"
+
+const router = express.Router();
+
+const requiredParams = ["type", "rhesus", "quantity"];
+
+router.post("/", validateRequestBody(requiredParams), BloodStorageService.set);
+router.get("/", BloodStorageService.get);
+
+export default router;
